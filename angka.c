4ace358183fec2ddb06d5341e89f167a61496693
kaishazizia <kaishazizia@gmail.com>
2026-04-13 15:22:24 +0700

    menambah file angka

---
+++ angka.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int angka;
+
+    printf("Masukkan sebuah angka: ");
+    scanf("%d", &angka);
+
+    if (angka > 0) {
+        printf("Angka %d adalah positif.\n", angka);
+    } else if (angka < 0) {
+        printf("Angka %d adalah negatif.\n", angka);
+    } else {
+        printf("Angka yang dimasukkan adalah nol.\n");
+    }
+
+    return 0;
+}
